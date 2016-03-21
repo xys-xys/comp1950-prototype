a320becf2591b28ae77edc7f95709505809471cb
Ehsan <ehsan.zb@gmail.com>
2016-03-20 18:38:57 -0700

    jQuery sub-menu added

---
+++ js/menu.js
@@ -0,0 +1,52 @@
+(function(){
+    
+    var $topMenuLinks = $('div.topNav nav.topMenu ul li a'),
+        $studentLink = $('nav.topMenu >ul >li').last(),
+        $studentSubMenu = $studentLink.children('ul.sub-menu'),
+        $studentSBlinks = $('div.topNav nav.topMenu ul li > ul.sub-menu > li > a');
+    
+    
+    $studentSubMenu.hide();
+    
+    $studentLink.on('click', function(event){
+        $studentSubMenu.slideToggle(300);
+        
+        event.preventDefault(); 
+    });
+    
+    
+    $studentSBlinks.eq(0).on('click', function(event){
+        
+        
+        event.preventDefault(); 
+    });
+    
+    
+    
+    function clearClick(){
+        
+        var itemsLn = $topMenuLinks.length;
+        
+        for (var i=0;i<itemsLn;i++){
+            var index = $(this).index();
+            $topMenuLinks.eq(i).css({
+                'color' : '#FFF',
+                backgroundColor : 'transparent'
+            });
+        
+        }
+    }
+    
+    $topMenuLinks.on('click',function(e){
+        clearClick();
+        $(this).css({
+            'color' : '#169E8D',
+            backgroundColor : 'black'
+        });
+        
+        e.preventDefault();
+    });
+    
+
+    
+})();
